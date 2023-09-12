91935b32a702989b5ff10b8d4707905ba92a755a
shrishti.eth <87112793+shrishtieth@users.noreply.github.com>
2023-09-12 17:29:13 +0530

    Update referal.sol

--- referal.sol
+++ referal.sol
@@ -1,36 +1,45 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8 .9;
+pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract Referal is Ownable {
+   
 
+    mapping(address => address) public referedBy;
+    mapping(address => address[]) public getRefrees;
+    mapping(address => uint256) public referredTime;
+    mapping(address => bool) public isWhitelisted;
+    
 
-	mapping(address => address) public referedBy;
-	mapping(address => address[]) public getRefrees;
-	mapping(address => uint256) public referredTime;
+    event Registered(address indexed user, address indexed referrer);
 
+  
+    constructor() {
+        isWhitelisted[0xFCAd0B19bB29D4674531d6f115237E16AfCE377c] = true;
+    }
+      
+    function register(address referrer) external{
+        require(referrer!= msg.sender,"User can't refer himself");
+        require(referedBy[msg.sender] == address(0),"Already registered");
+        referedBy[msg.sender] = referrer;
+        getRefrees[referrer].push(msg.sender);
+        referredTime[msg.sender] = block.timestamp;
+        emit Registered(msg.sender, referrer);
+    }
 
-	event Registered(address indexed user, address indexed referrer);
+    function getReferrer(address user) external  view returns(address){
+        return(referedBy[user]);
+    }
 
+    function getAllRefrees(address user) external  view returns(address[] memory){
+        return(getRefrees[user]);
+    }        
 
-	constructor() {}
-
-	function register(address referrer) external {
-		require(referrer != msg.sender, "User can't refer himself");
-		require(referedBy[msg.sender] == address(0), "Already registered");
-		referedBy[msg.sender] = referrer;
-		getRefrees[referrer].push(msg.sender);
-		referredTime[msg.sender] = block.timestamp;
-		emit Registered(msg.sender, referrer);
-	}
-
-	function getReferrer(address user) external view returns(address) {
-		return (referedBy[user]);
-	}
-
-	function getAllRefrees(address user) external view returns(address[] memory) {
-		return (getRefrees[user]);
-	}
+    function handleWhitelist(address[] memory user, bool isTrue) external onlyOwner {
+        for(uint256 i=0; i<user.length; i++){
+            isWhitelisted[user[i]] = isTrue;
+        }
+    }                                               
 }
